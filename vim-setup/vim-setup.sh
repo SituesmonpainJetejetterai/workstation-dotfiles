870d90ed9376dd467bfd79758e605f36c36bebdd
jetejetteraiASAP <situmonpainjetejetterai@gmail.com>
2021-07-11 20:18:57 +0530

    tried to change the logic a bit, didn't work

--- vim-setup/vim-setup.sh
+++ vim-setup/vim-setup.sh
@@ -4,22 +4,22 @@
 # The .vim/ directory is already created and is ready to be copied into $HOME
 # If not present, it will copy the entire folder, essentially making vim workable, with the required config.
 
-# While using vim on the machine, I might make chanes. Instead of copying each file by hand, I'll just copy everything inside $HOME/.vim here, and push. Easy.
+# While using vim on the machine, I might make changes. Instead of copying each file by hand, I'll just copy everything inside $HOME/.vim here, and push. Easy.
 
 # use chmod u+x script to make it executable.
 
-DEST=$HOME/.vim
+DEST="$HOME/.vim/"
 
-if ! [[ -f $DEST ]]; then
-  cp .vim ~/.
+if ! [[ -d $DEST ]]; then
+  cp -r "./.vim/" "$HOME/"
 fi
 
-SRC=./
+SRC=".vim/"
 
 read -n 1 -p "Copy changes from ~/.vim to .vim? [Y/N] " reply;
 if [ "$reply" != "" ]; then echo; fi
 if [ "$reply" = "${reply#[Nn]}" ]; then echo; fi
-if [ "$reply" = "${reply#[Yy]}" ]; 
-    rm -rf .vim && cp -r $HOME/.vim 
+if [ "$reply" = "${reply#[Yy]}" ]; then 
+    rm -rf .vim/ && mkdir .vim/ && cp -r $HOME/.vim/* .vim/
 fi
 
