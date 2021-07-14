e53db5b85c97ac97e73ff6886dd8e0c2821e32c8
jetejetteraiASAP <situmonpainjetejetterai@gmail.com>
2021-07-14 23:11:31 +0530

    works perfectly now

--- vim-setup/vim-setup.sh
+++ vim-setup/vim-setup.sh
@@ -11,7 +11,7 @@ echo 'If you want to copy ~/.vim/ in .vim, press 2'
 read -p "What do you want to do?: " option
 
 case "$option" in 
-    "1") rsync -anv .vim/ ~/.vim;;
-    "2") rsync -anv ~/.vim/ .vim;;
+    "1") rsync -av .vim/ ~/.vim;;
+    "2") rsync -av ~/.vim/ .vim;;
     *) echo "You're stupid!";;
 esac
