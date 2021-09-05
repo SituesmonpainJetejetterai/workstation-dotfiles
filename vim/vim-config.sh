632ab4c16514a1d6f2b23d3498910118537a530c
jetejetteraiASAP <situmonpainjetejetterai@gmail.com>
2021-09-06 00:55:05 +0530

    added installation command for vim-gtk

--- vim/vim-config.sh
+++ vim/vim-config.sh
@@ -5,17 +5,21 @@ if [[ $PWD == ~ ]]; then
     exit 1
 fi 
 
-dot=$(dirname "$0")
-colour_path="$dot/.vim/colors/"
+echo 'Installing/updating vim with features'
+sudo apt install vim-gtk -y
+
+# dot=$(dirname "$0")
+colour_path="$(dirname "$0")/.vim/colors/"
 
 echo 'updating gruvbox.vim'
 
-cd || return "$colour_path" && { curl -O https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim; cd -; }
+cd || return "$colour_path" && { curl -O https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim; cd - || return; }
 
 echo "updating config in $HOME/.vim"
 
+cd "$HOME" || exit
 if [ -L "$HOME/.vim" ] && [ -e "$HOME/.vim" ]; then
     echo 'Link exists'
 else
-    ln -s "$dot/.vim" "$HOME"
+    ln -s "$HOME/git-repos/setups/vim/.vim" "$HOME"
 fi
