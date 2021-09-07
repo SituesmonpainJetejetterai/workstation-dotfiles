8481ae63821a54f222d48ed432f98592eb90594e
jetejetteraiASAP <situmonpainjetejetterai@gmail.com>
2021-09-07 13:51:25 +0530

    added comments

--- bash/.bash/.bash_functions.sh
+++ bash/.bash/.bash_functions.sh
@@ -2,22 +2,46 @@
 
 # Setting functions
 
-## Setting git functions
+
+# ---
+
+
+
+# Setting git functions
 
 parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 export PS1="\u@\h \[\e[32m\]\w \[\e[91m\]\$(parse_git_branch)\[\e[00m\]$ "
 
-## apt function
 
-### The "$1" parameter takes the first argument passed to the function in the shell as input
+
+# ---
+
+
+
+
+# apt function
+
+## The "$1" parameter takes the first argument passed to the function in the shell as input
+
+## install a package
 ins()
 {
     sudo apt install "$1" -y
 }
 
+## purge an application & cleanup
 del ()
 {
     sudo apt purge "$1" -y && sudo apt autoremove -y
 }
+
+
+
+# ---
+
+
+
+
+
