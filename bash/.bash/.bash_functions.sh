546c5beddcc875d20725f0f30b8b3d004673120a
jetejetteraiASAP <situmonpainjetejetterai@gmail.com>
2021-09-07 12:48:16 +0530

    Added functions to install and purge applications without writing long commands, basically a better alias

--- bash/.bash/.bash_functions.sh
+++ bash/.bash/.bash_functions.sh
@@ -8,3 +8,15 @@ parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 export PS1="\u@\h \[\e[32m\]\w \[\e[91m\]\$(parse_git_branch)\[\e[00m\]$ "
+
+## apt function
+
+ins()
+{
+    sudo apt install "$1" -y
+}
+
+del ()
+{
+    sudo apt purge "$1" -y && sudo apt autoremove -y
+}
