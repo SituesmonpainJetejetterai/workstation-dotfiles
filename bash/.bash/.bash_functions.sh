13211c8c891a0b7a111c2cc3b2ca47790d67b14a
jetejetteraiASAP <situmonpainjetejetterai@gmail.com>
2021-09-10 15:16:49 +0530

    feat: function to search text in files; refactor: comments
    Added a function to search for text in files inside the current folder.
    Using `find`, `xargs` and `grep`. Can handle subdirectories. Refactored
    some comments.

--- bash/.bash/.bash_functions.sh
+++ bash/.bash/.bash_functions.sh
@@ -13,25 +13,37 @@ function hg () {
     history | grep "${1}" | less
 }
 
+# search for text in files inside current folder
+# add one other parameter (generally `w` to match separate words) to `grep`
+# `sed G` simply appends a newline character followed by the contents of the hold space to the pattern space.
+function search () {
+    if [ -z "${2}" ];
+    then
+        find . -type f -print0 | xargs -0 -I /{} grep -H "${1}" /{} | sed G | less
+    else
+        find . -type f -print0 | xargs -0 -I /{} grep -H"${2}" "${1}" /{} | sed G | less
+    fi
+}
+
 # Git functions
 
-## Shows the branch I'm currently on while inside a git repository
-function parse_git_branch() {
+## Show the branch I'm currently on while inside a git repository
+function parse_git_branch () {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 export PS1="\u@\h \[\e[32m\]\w \[\e[91m\]\$(parse_git_branch)\[\e[00m\]$ "
 
-## Shows the git diff in a colourful pager
+## Show the git diff in a colourful pager
 function gd () {
     git diff "${1-.}"
 }
 
-## Merges remote changes with the local branch
+## Merge remote changes with the local branch
 function gfm () {
     git fetch origin "${1}" && git merge -s recursive -X theirs origin/"${1}"
 }
 
-## Performs the git action on all subdirectories with .git/ in them.
+## Perform the git action on all subdirectories with .git/ in them.
 ## Help: https://stackoverflow.com/questions/3497123/run-git-pull-over-all-subdirectories
 function gall () {
     find . -type d -name ".git" -printf "%h: " -prune -exec git --git-dir={} "${1}" \;
