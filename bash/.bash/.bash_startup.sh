00c8620e08471d6b4a3e434127734d1939cae16a
jetejetteraiASAP <situmonpainjetejetterai@gmail.com>
2021-10-13 20:27:20 +0530

    refactor: remove text, edit comments

--- bash/.bash/.bash_startup.sh
+++ bash/.bash/.bash_startup.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env sh
 
 # Set the terminal to show colours
-
 export TERM=xterm-256color
 
-# Showing system information
+# Clear the screen when executing the bash shell. Useful when I'm sourcing the .bashrc
 clear
-cd "$HOME" || return
 
+# Showing system information
 printf "PUBLIC IP: %s\n" "$(curl -sS ifconfig.me)"
 printf "USER: %s\n" "$USER"
 printf "TIME: %s\n" "$(date '+%a %d %b %T %Y %Z')"
@@ -19,7 +18,7 @@ printf "PACKAGES: %s\n" "$(dpkg --get-selections | wc -l)"
 printf "MEMORY: %s\n" "$(free -m -h | awk '/Mem/{print $3"/"$2}')"
 printf "\n"
 
-# Show the PS1
+# Show a custom PS1
 output_PS1() {
     git_info() {
         git_branch() {
@@ -54,48 +53,3 @@ output_PS1
 
 # Set vim keys for bash
 set -o vi
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# printf "-------------------------------System Information----------------------------"
-# printf "Hostname:\t\t"$(hostname)
-# printf "uptime:\t\t\t"$(uptime | awk '{print $3,$4}' | sed 's/,//')
-# printf "Manufacturer:\t\t"`cat /sys/class/dmi/id/chassis_vendor`
-# printf "Product Name:\t\t"`cat /sys/class/dmi/id/product_name`
-# printf "Version:\t\t"`cat /sys/class/dmi/id/product_version`
-# printf "Serial Number:\t\t"`cat /sys/class/dmi/id/product_serial`
-# printf "Machine Type:\t\t"$(vserver=$(lscpu | grep Hypervisor | wc -l); if [ $vserver -gt 0 ]; then echo "VM"; else echo "Physical"; fi)
-# printf "Operating System:\t"`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`
-# printf "Kernel:\t\t\t"`uname -r`
-# printf "Architecture:\t\t"`arch`
-# printf "Processor Name:\t\t"`awk -F':' '/^model name/ {print $2}' /proc/cpuinfo | uniq | sed -e 's/^[ \t]*//'`
-# printf "Active User:\t\t"`w | cut -d ' ' -f1 | grep -v USER | xargs -n1`
-# printf "System Main IP:\t\t"`hostname -I`
-# printf "Public IP:\t\t"`curl -sS ifconfig.me`
-# echo ""
-# printf "-------------------------------CPU/Memory Usage------------------------------"
-# printf "Memory Usage:\t"`free | awk '/Mem/{printf("%.2f%"), $3/$2*100}'`
-# printf "Swap Usage:\t"`free | awk '/Swap/{printf("%.2f%"), $3/$2*100}'`
-# printf "CPU Usage:\t"`cat /proc/stat | awk '/cpu/{printf("%.2f%\n"), ($2+$4)*100/($2+$4+$5)}' |  awk '{print $0}' | head -1`
-# echo ""
-# printf "-------------------------------Disk Usage >80%-------------------------------"
-# df -Ph | sed s/%//g | awk '{ if($5 > 80) print $0;}'
-# echo ""
-
-# set -o vi
-
-
