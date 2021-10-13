cab774a5e998934a99a335b4ca1042408e19ea13
jetejetteraiASAP <situmonpainjetejetterai@gmail.com>
2021-10-13 09:53:20 +0530

    refactor: quotes, change to `printf`, CPU

--- bash/.bash/.bash_startup.sh
+++ bash/.bash/.bash_startup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Set the terminal to show colours
 
@@ -8,16 +8,16 @@ export TERM=xterm-256color
 clear
 cd "$HOME" || return
 
-echo -e "Public IP: $(curl -sS ifconfig.me)"
-echo -e "User: $USER"
-echo -e "TIME: $(date '+%a %d %b %T %Y %Z')"
-echo -e "UPTIME: $(uptime -p)"
-# echo -e "CPU: $(awk -F: '/model name/{print $2}' | head -1)"
-echo -e "CPU Usage: $(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')"
-echo -e "KERNEL: $(uname -rms)"
-echo -e "PACKAGES: $(dpkg --get-selections | wc -l)"
-echo -e "MEMORY: $(free -m -h | awk '/Mem/{print $3"/"$2}')"
-echo ""
+printf "PUBLIC IP: %s\n" "$(curl -sS ifconfig.me)"
+printf "USER: %s\n" "$USER"
+printf "TIME: %s\n" "$(date '+%a %d %b %T %Y %Z')"
+printf "UPTIME: %s\n" "$(uptime -p)"
+printf "CPU: %s\n" "$(lscpu | grep "Model\sname" | sed "s/.*:\s*//")"
+printf "CPU Usage: %s\n" "$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')"
+printf "KERNEL: %s\n" "$(uname -rms)"
+printf "PACKAGES: %s\n" "$(dpkg --get-selections | wc -l)"
+printf "MEMORY: %s\n" "$(free -m -h | awk '/Mem/{print $3"/"$2}')"
+printf "\n"
 
 # Show the PS1
 output_PS1() {
@@ -45,7 +45,7 @@ output_PS1() {
     }
 
     error_code() {
-        printf "$?"
+        printf "%s" "$?"
     }
 
     export PS1="\n\[$(tput setab 7)$(tput setaf 1)\]\$(error_code)\[$(tput setab 0)$(tput setaf 3)\] \u\[$(tput setb 0)$(tput setaf 7)\]@\[$(tput setb 2)$(tput setaf 6)\]\h\[$(tput setb 6)$(tput setaf 2)\] -> \[$(tput setb 6)$(tput setaf 8)\]\w\[$(tput setaf 1)\] \$(git_info)\n\[$(tput bold)$(tput setaf 4)\]\_$ \[$(tput sgr0)\]"
@@ -71,28 +71,28 @@ set -o vi
 
 
 
-# echo -e "-------------------------------System Information----------------------------"
-# echo -e "Hostname:\t\t"$(hostname)
-# echo -e "uptime:\t\t\t"$(uptime | awk '{print $3,$4}' | sed 's/,//')
-# echo -e "Manufacturer:\t\t"`cat /sys/class/dmi/id/chassis_vendor`
-# echo -e "Product Name:\t\t"`cat /sys/class/dmi/id/product_name`
-# echo -e "Version:\t\t"`cat /sys/class/dmi/id/product_version`
-# echo -e "Serial Number:\t\t"`cat /sys/class/dmi/id/product_serial`
-# echo -e "Machine Type:\t\t"$(vserver=$(lscpu | grep Hypervisor | wc -l); if [ $vserver -gt 0 ]; then echo "VM"; else echo "Physical"; fi)
-# echo -e "Operating System:\t"`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`
-# echo -e "Kernel:\t\t\t"`uname -r`
-# echo -e "Architecture:\t\t"`arch`
-# echo -e "Processor Name:\t\t"`awk -F':' '/^model name/ {print $2}' /proc/cpuinfo | uniq | sed -e 's/^[ \t]*//'`
-# echo -e "Active User:\t\t"`w | cut -d ' ' -f1 | grep -v USER | xargs -n1`
-# echo -e "System Main IP:\t\t"`hostname -I`
-# echo -e "Public IP:\t\t"`curl -sS ifconfig.me`
+# printf "-------------------------------System Information----------------------------"
+# printf "Hostname:\t\t"$(hostname)
+# printf "uptime:\t\t\t"$(uptime | awk '{print $3,$4}' | sed 's/,//')
+# printf "Manufacturer:\t\t"`cat /sys/class/dmi/id/chassis_vendor`
+# printf "Product Name:\t\t"`cat /sys/class/dmi/id/product_name`
+# printf "Version:\t\t"`cat /sys/class/dmi/id/product_version`
+# printf "Serial Number:\t\t"`cat /sys/class/dmi/id/product_serial`
+# printf "Machine Type:\t\t"$(vserver=$(lscpu | grep Hypervisor | wc -l); if [ $vserver -gt 0 ]; then echo "VM"; else echo "Physical"; fi)
+# printf "Operating System:\t"`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`
+# printf "Kernel:\t\t\t"`uname -r`
+# printf "Architecture:\t\t"`arch`
+# printf "Processor Name:\t\t"`awk -F':' '/^model name/ {print $2}' /proc/cpuinfo | uniq | sed -e 's/^[ \t]*//'`
+# printf "Active User:\t\t"`w | cut -d ' ' -f1 | grep -v USER | xargs -n1`
+# printf "System Main IP:\t\t"`hostname -I`
+# printf "Public IP:\t\t"`curl -sS ifconfig.me`
 # echo ""
-# echo -e "-------------------------------CPU/Memory Usage------------------------------"
-# echo -e "Memory Usage:\t"`free | awk '/Mem/{printf("%.2f%"), $3/$2*100}'`
-# echo -e "Swap Usage:\t"`free | awk '/Swap/{printf("%.2f%"), $3/$2*100}'`
-# echo -e "CPU Usage:\t"`cat /proc/stat | awk '/cpu/{printf("%.2f%\n"), ($2+$4)*100/($2+$4+$5)}' |  awk '{print $0}' | head -1`
+# printf "-------------------------------CPU/Memory Usage------------------------------"
+# printf "Memory Usage:\t"`free | awk '/Mem/{printf("%.2f%"), $3/$2*100}'`
+# printf "Swap Usage:\t"`free | awk '/Swap/{printf("%.2f%"), $3/$2*100}'`
+# printf "CPU Usage:\t"`cat /proc/stat | awk '/cpu/{printf("%.2f%\n"), ($2+$4)*100/($2+$4+$5)}' |  awk '{print $0}' | head -1`
 # echo ""
-# echo -e "-------------------------------Disk Usage >80%-------------------------------"
+# printf "-------------------------------Disk Usage >80%-------------------------------"
 # df -Ph | sed s/%//g | awk '{ if($5 > 80) print $0;}'
 # echo ""
 
