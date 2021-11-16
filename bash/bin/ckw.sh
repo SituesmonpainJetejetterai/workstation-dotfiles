c3c10e93beb324d45f6a27aacfc5efc901f0c9bc
jetejetteraiASAP <situmonpainjetejetterai@gmail.com>
2021-11-16 20:50:01 +0530

    refactor: "type" is not working, replace with custom logic

--- bash/bin/ckw.sh
+++ bash/bin/ckw.sh
@@ -5,8 +5,10 @@
 ckw() {
     # Bash functions defined in the interactive shell
     # Supress all errors, display through less if output doesn't fit the screen
-    type "${1}" 2>/dev/null | less -FXR
-    grep -Hwne "${1}" "$HOME/bin/.bash/.bash_functions.sh" --color=always | tr -d "{"
+#     type "${1}" | less -FXR 2>/dev/null
+    find . -name "*${1}*" -exec less -FXR {} \; 2>/dev/null
+    grep -Hwne "${1}" "$HOME/bin/.bash/.bash_functions.sh" --color=always 2>/dev/null | tr -d "{"
+    grep -Hwne "alias\s${1}" "$HOME/bin/.bash/.bash_aliases" --color=always 2>/dev/null
 
     # Show matches from vimrc
     grep -HEn "remap|command!" "$HOME/.vim/vimrc" | sed -e 's/\s*\".*//; /^$/d' | grep -w "${1}" --color=always | less -FXR
