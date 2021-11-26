38515ea353990a211166255f567c6767229cbc2c
Debian <admin@ip-172-31-85-116.ec2.internal>
2021-11-26 09:07:31 +0000

    refactor: comments, set "PATH", show "PATH"

--- setup.sh
+++ setup.sh
@@ -3,42 +3,37 @@
 # Updating the system and installing some applications.
 
 printf "\n%s\n\n" "Updating the system, removing vim and installing applications"
-sudo apt update -y && sudo apt upgrade -y
+sudo apt update -y && sudo apt-get full-upgrade -y
 printf "\n"
 sudo apt purge vim -y && sudo apt autoremove -y
 printf "\n"
 sudo apt install tmux vim-gtk tree htop git shellcheck pylint python3 less curl -y
 printf "\n"
 
-# if [[ $PWD == ~ ]]; then
-    # echo 'Running this script from your home dir is pointless.'
-    # exit 1
-# fi
-
 # variable
 setup_path="$(dirname "$(readlink -f "$0")")"
 
 # vim setup
 
-printf "\n%s\n" "updating config in ~/.vim"
+printf "\n%s\n" "symlinking vim config in ~/.vim"
 ln -sf "${setup_path}/vim/.vim" "$HOME"
 
 # tmux setup
 
-printf "\n%s\n" "copying over the tmux configuration file"
+printf "\n%s\n" "symlinking the tmux configuration file"
 ln -sf "${setup_path}/tmux/.tmux.conf" "$HOME"
 
 # git setup
 
-printf "\n%s\n" "copying over the global git config file"
+printf "\n%s\n" "symlinking the global git config file"
 ln -sf "${setup_path}/git/.gitconfig" "$HOME"
 
 # bash setup
 
-printf "\n%s\n" "copying over the scripts for bash configuration"
+printf "\n%s\n" "symlinking the scripts for bash configuration"
 ln -sf "${setup_path}/bash/bin" "$HOME"
 
-printf "\n%s\n\n" "adding lines to ~/.bashrc to source the scripts"
+printf "\n%s\n" "adding lines to ~/.bashrc to source the scripts"
 
 ## Escaping the character ${f} with \ helps to keep its literal value, i.e. outputs it as the string: "${f}"
 
@@ -49,7 +44,9 @@ for f in $HOME/bin/.bash/.*
 do
     if [ ! -d "\${f}" ]; then source "\${f}"; fi
 done
+export PATH=$PATH:$HOME/bin
 EOT
 )
 
 grep -qx "${string}" "$HOME/.bashrc" || echo "${string}" >> "$HOME/.bashrc"
+printf "\n%s%s" "This is the current path: " "$PATH"
