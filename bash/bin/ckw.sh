c22cf06fe971905a75600bfa5796d2357ad3a3ce
jetejetteraiASAP <situmonpainjetejetterai@gmail.com>
2021-11-30 11:31:31 +0000

    fix: miscellaneous edits to "ckw.sh"

--- bash/bin/ckw.sh
+++ bash/bin/ckw.sh
@@ -3,10 +3,10 @@
 word="${1}"
 
 # If a script of that name exists
-find "$HOME" -path "*/bin/*" -type f -name "*${word}*" -exec less -FXR {} \; && printf "\n%s%s\n" "${word}" ".sh is a shell script"
+find "$HOME" -path "*/bin/*" -type f -name "*${word}*" -print0 | xargs -0 -r less -FXR
 
 # If a function of that name exists in ".bash_functions.sh"
-sed -n "/^${word}()/,/^}/p" "$HOME/bin/.bash/.bash_functions.sh" | xargs -0 -I {} printf "\n%s\n%s" "${word} is a function defined in the interactive shell" "{}" 2> /dev/null
+sed -n "/^${word}()/,/^}/p" "$HOME/bin/.bash/.bash_functions.sh" | less -FXR 2> /dev/null
 
 # If an alias of that name exists in ".bash_aliases"
-grep -Hwne "alias\s${word}" "$HOME/bin/.bash/.bash_aliases" --color=always | xargs -0 -I {} printf "\n%s%s\n" "Alias:~ " "{}" 2> /dev/null
+grep -Hwne "alias\s${word}" "$HOME/bin/.bash/.bash_aliases" --color=always | xargs -0 -I {} printf "\n%s\n%s\n" "Alias:~ " "{}" 2> /dev/null
