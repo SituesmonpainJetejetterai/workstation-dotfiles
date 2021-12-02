e8060b0a73a41b7567541bf081fb913874d1b8cb
jetejetteraiASAP <situmonpainjetejetterai@gmail.com>
2021-12-02 10:27:57 +0530

    refactor: proper conditions for "if" statement

--- bash/bin/gdf.sh
+++ bash/bin/gdf.sh
@@ -4,13 +4,13 @@
 # Optional argument to specify the branch, otherwise will act on current branch
 gdf() {
     printf "\n%s" "Enter branch to reset from. Default is current branch: "
-    if read -r branch; then
+    read -r branch
+    if [ "${branch}" != "" ]; then
         # Override local edits and reset hard with remote changes
         git fetch --all && git reset --hard origin/"${branch}"
     else
         # Get the current branch
-        current_branch="$(git rev-parse --abbrev-ref HEAD)"
-        git fetch --all && git reset --hard origin/"${current_branch:-main}"
+        git fetch --all && git reset --hard origin\/"$(git rev-parse --abbrev-ref HEAD)"
     fi
 }
 
