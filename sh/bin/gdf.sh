5604bfa1231cce2bed5a2eb2b81f537727b11503
jetejetteraiASAP <situmonpainjetejetterai@gmail.com>
2021-12-14 13:09:24 +0000

    refactor: allow for a direct argument with function

--- sh/bin/gdf.sh
+++ sh/bin/gdf.sh
@@ -3,15 +3,19 @@
 # Forcibly pull remote changes and override local changes
 # Optional argument to specify the branch, otherwise will act on current branch
 gdf() {
-    printf "\n%s" "Enter branch to reset from. Default is current branch: "
-    read -r branch
-    if [ "${branch}" != "" ]; then
-        # Override local edits and reset hard with remote changes
-        git fetch --all && git reset --hard origin/"${branch}"
+    if [ -z "${1}" ]; then
+        printf "\n%s" "Enter branch to reset from. Default is current branch: "
+        read -r branch
+        if [ "${branch}" != "" ]; then
+            # Override local edits and reset hard with remote changes
+            git fetch --all && git reset --hard origin\/"${branch}"
+        else
+            # Get the current branch
+            git fetch --all && git reset --hard origin\/"$(git rev-parse --abbrev-ref HEAD)"
+        fi
     else
-        # Get the current branch
-        git fetch --all && git reset --hard origin\/"$(git rev-parse --abbrev-ref HEAD)"
+        git fetch --all && git reset --hard origin\/"${1}"
     fi
 }
 
-gdf
+gdf $@
