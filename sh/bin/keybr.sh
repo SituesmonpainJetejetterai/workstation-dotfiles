982f437f27fbe9178076ddb362677634903f2000
jetejetteraiASAP <situmonpainjetejetterai@gmail.com>
2021-12-15 22:19:10 +0530

    feat: add easier level to "keybr.sh"

--- sh/bin/keybr.sh
+++ sh/bin/keybr.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 printf "\n%s" "Welcome to a keyboard practice session"
-printf "\n%s" "Press \"1\" for level 1, \"2\" for level 2, and \"3\" for level 3: "
+printf "\n%s" "Press \"1\" for level 1, \"2\" for level 2, \"3\" for level 3, and \"4\" for level 4: "
 if read -r level; then
     case ${level} in
         1)
             while true;
             do
-                attempt="$(head /dev/urandom | tr -dc 'A-Za-z' | head -c15)"
+                attempt="$(head /dev/urandom | tr -dc 'a-z' | head -c10)"
                 printf "\n%s" "${attempt}"
                 printf "\n%s" "Enter: "
                 read -r word
@@ -21,7 +21,7 @@ if read -r level; then
         2)
             while true;
             do
-                attempt="$(head /dev/urandom | tr -dc 'A-Za-z0-9'! | head -c15)"
+                attempt="$(head /dev/urandom | tr -dc 'A-Za-z' | head -c15)"
                 printf "\n%s" "${attempt}"
                 printf "\n%s" "Enter: "
                 read -r word
@@ -33,6 +33,20 @@ if read -r level; then
             done
             ;;
         3)
+            while true;
+            do
+                attempt="$(head /dev/urandom | tr -dc 'A-Za-z0-9'! | head -c15)"
+                printf "\n%s" "${attempt}"
+                printf "\n%s" "Enter: "
+                read -r word
+                while [ "${word}" != "${attempt}" ];
+                do
+                    printf "\n%s" "Enter: "
+                    read -r word
+                done
+            done
+            ;;
+        4)
             while true;
             do
                 attempt="$(head /dev/urandom | tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' | head -c25)"
